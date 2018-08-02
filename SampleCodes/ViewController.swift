210564caaca9822507fba76695e05e5618ed297c
iamwee <wathin.v@gmail.com>
2018-08-02 13:56:17 +0700

    Initial Commit

---
+++ SampleCodes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleCodes
+//
+//  Created by Wathin Sonnukij on 2/8/2561 BE.
+//  Copyright © 2561 Wathin Sonnukij. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
